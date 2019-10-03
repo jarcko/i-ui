fce5d9a4c04640d94a605b1c69e5642a3d5d1b2e
igor.jarko@thomascookonline.com <Jarwaves2511>
2019-10-04 02:13:20 +0300

    subscriptions fix

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,20 +1,22 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
 import { FeedItem } from 'src/app/core/interfaces';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { FeedService } from 'src/app/core/feed.service';
 import { DeviceService } from 'src/app/core/device.service';
+import {takeUntil} from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   feedItems: FeedItem[];
   isAllContentLoaded: boolean;
   error$: Observable<Error>;
+  unsubscribe$ = new Subject<void>();
 
   constructor(
     private route: ActivatedRoute,
@@ -24,7 +26,10 @@ export class HomeComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.route.data.pipe(map(({ resolved }) => resolved))
+    this.route.data.pipe(
+      takeUntil(this.unsubscribe$),
+      map(({ resolved }) => resolved)
+    )
       .subscribe(items => this.feedItems = items);
     this.error$ = this.feedService.error$;
   }
@@ -32,13 +37,18 @@ export class HomeComponent implements OnInit {
   onLoadMore(): void {
     if (!this.isAllContentLoaded) {
       const index = this.feedItems.length.toString();
-      this.feedService.fetchFeedItems(index, this.deviceService.itemsQty.toString())
+      const quantity = this.deviceService.itemsQty.toString();
+      this.feedService.fetchFeedItems(index, quantity).pipe(takeUntil(this.unsubscribe$))
         .subscribe(items => {
           this.feedItems = [...this.feedItems, ...items];
           this.isAllContentLoaded = !items.length;
           this.cdr.detectChanges();
         });
     }
+  }
 
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
